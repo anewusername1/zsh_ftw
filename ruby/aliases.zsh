ca8d75d6a37132e65fb49432a4d513495bd20feb
Tracey Eubanks <traceyeubanks@yahoo.com>
2012-01-17 09:59:56 -0700

    removed symlink for rspec

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,4 +1,3 @@
-alias specit='bundle exec rspec spec'
 alias testall='echo "running rspec" && bundle exec rspec spec && echo "running cucumber" && cucumber'
 alias testify='watchr ~/.watchr'
 alias bx='bundle exec'
